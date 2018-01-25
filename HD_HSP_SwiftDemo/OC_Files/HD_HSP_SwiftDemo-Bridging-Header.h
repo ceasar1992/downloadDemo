ccd899d60dfb7b8c883b75ef238b5bb1c1aa7093
郝鹏 <haoceasar@163.com>
2018-01-25 14:58:20 +0800

    delete almofire

---
+++ HD_HSP_SwiftDemo/OC_Files/HD_HSP_SwiftDemo-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  HD_HSP_SwiftDemo-Bridging-Header.h
+//  HD_HSP_SwiftDemo
+//
+//  Created by HDHaoShaoPeng on 2018/1/25.
+//  Copyright © 2018年 HDHaoShaoPeng. All rights reserved.
+//
+
+#ifndef HD_HSP_SwiftDemo_Bridging_Header_h
+#define HD_HSP_SwiftDemo_Bridging_Header_h
+
+
+#endif /* HD_HSP_SwiftDemo_Bridging_Header_h */
