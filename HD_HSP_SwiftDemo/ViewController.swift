0086708b788625dae966be88df4fec20bd7768d1
郝鹏 <haoceasar@163.com>
2018-01-25 14:06:09 +0800

    init commit

---
+++ HD_HSP_SwiftDemo/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  HD_HSP_SwiftDemo
+//
+//  Created by HDHaoShaoPeng on 2017/9/19.
+//  Copyright © 2017年 HDHaoShaoPeng. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
